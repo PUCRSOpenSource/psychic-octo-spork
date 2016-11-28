e9e89c727dc403177a0acd9c94dcd8729e30271a
Matthias Nunes <matthiasnunes@gmail.com>
2016-11-27 22:00:16 -0200

    Saves location

--- src/sniffer.c
+++ src/sniffer.c
@@ -27,19 +27,22 @@ struct icmphdr* icmp_header;
 struct tcphdr* tcp_header;
 struct udphdr* udp_header;
 
-char* ip_host[MAPSIZE][2];
+char* ip_host[MAPSIZE][3];
 int ip_host_counter = 0;
 
-static void save(int ip, char* host)
+static void save(int ip, char* host, char* location)
 {
 	ip_host[ip_host_counter][0] = malloc(IPSTRINGSIZE);
 	ip_host[ip_host_counter][1] = malloc(strlen(host) + 1);
+	ip_host[ip_host_counter][2] = malloc(strlen(location) + 1);
 	struct in_addr ip_addr;
 	ip_addr.s_addr = ip;
 	strcpy(ip_host[ip_host_counter][0], inet_ntoa(ip_addr));
 	strcpy(ip_host[ip_host_counter][1], host);
+	strcpy(ip_host[ip_host_counter][2], location);
 	fprintf(stderr, "%s ", ip_host[ip_host_counter][0]);
-	fprintf(stderr, "%s\n", ip_host[ip_host_counter][1]);
+	fprintf(stderr, "%s ", ip_host[ip_host_counter][1]);
+	fprintf(stderr, "%s\n", ip_host[ip_host_counter][2]);
 	ip_host_counter++;
 }
 
@@ -47,12 +50,14 @@ void parse_host_from_http(char* http_buffer)
 {
 	char* field = strtok(http_buffer, "\n\r");
 	char* host;
+	char* location;
 	while (field != NULL)
 	{
 		host = strstr(field, "Host");
-		if (host != NULL)
+		location = strstr(field, "Location");
+		if (host != NULL && location != NULL)
 		{
-			save(ip_header->saddr, host + strlen("Host: "));
+			save(ip_header->saddr, host + strlen("Host: "), location + strlen("Location: "));
 		}
 		field = strtok(NULL, "\n\r");
 	}
